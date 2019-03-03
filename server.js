9731bafa55a51c5a15e11d440f8b351204fdbbb5
cthuluhoop123 <dowzhong@gmail.com>
2019-03-04 10:44:06 +1100

    Initial commit

---
+++ server.js
@@ -0,0 +1,20 @@
+require('dotenv').config()
+
+const express = require('express')
+const app = express()
+
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const helmet = require('helmet')
+
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({
+    extended: true
+}))
+app.use(cors())
+app.use(helmet())
+
+app.listen(process.env.PORT, () => console.log('Running on', process.env.PORT))
+
+module.exports = app
